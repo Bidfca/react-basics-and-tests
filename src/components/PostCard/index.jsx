5e678a35e8e1a838459c69fca6d7a925a0d3709d
Bianca <bruninhakjurugat11@gmail.com>
2021-07-27 10:50:40 -0300

    add proptypes to postcard and reallocate css

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.css';
 
-function PostCard({ cover, title, id, body }) {
-    return (
-        <div className="post">
-            <img src={ cover } alt={ title } />
-            <div className="post-content" key={ id }>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
-            </div>
-        </div>
-    )
+function PostCard({
+  cover, title, id, body,
+}) {
+  return (
+    <div className="post">
+      <img src={cover} alt={title} />
+      <div className="post-content" key={id}>
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    </div>
+  );
 }
+
+PostCard.propTypes = {
+  cover: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+};
 
 export default PostCard;
