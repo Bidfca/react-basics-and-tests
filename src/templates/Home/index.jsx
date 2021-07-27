5ad0b1f441e7c29b3dc82d5507615ee32527596c
Bianca <bruninhakjurugat11@gmail.com>
2021-07-27 10:53:37 -0300

    add pagination and reallocate css

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -2,10 +2,14 @@ import './styles.css';
 import React from 'react';
 import { loadPosts } from '../../utils/load-posts';
 import Posts from '../../components/Posts';
+import Button from '../../components/Button';
 
 class Home extends React.Component {
   state = {
-    posts: []
+    posts: [],
+    allPosts: [],
+    postsPerPage: 20,
+    page: 0
   }
 
   async componentDidMount() {
@@ -13,16 +17,48 @@ class Home extends React.Component {
   }
 
   loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
     const postsAndPhotos = await loadPosts();
-    this.setState({ posts: postsAndPhotos })
+
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+    })
+  }
+
+  loadMorePosts = () => {
+    const {
+      page,
+      postsPerPage,
+      allPosts,
+      posts,
+    } = this.state;
+
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
+
   }
 
   render() {
-    const { posts } = this.state;
+    const { posts, postsPerPage, allPosts, page } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
 
     return (
       <section className="container">
-        <Posts posts={ posts } />
+        <Posts posts={posts} />
+
+        <input onChange={}
+        type="search"></input>
+        <div class="button-container">
+          <Button
+            text="Load more posts"
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts} />
+        </div>
+
       </section>
     );
   }
