02252607bbe78dc9626ff6fa6427d0a574b2d5f5
Bianca <bruninhakjurugat11@gmail.com>
2021-07-27 10:51:29 -0300

    add proptypes to posts and reallocate css

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import PostCard from '../PostCard';
+import './styles.css';
 
 function Posts({ posts }) {
-    return (
-        <div className="posts">
-        {posts.map(({ cover, title, id, body }) => (
-          <PostCard
-            title={ title }
-            cover={ cover }
-            id={ id }
-            body={ body } />
-        ))}
-      </div>
-    )
+  return (
+    <div className="posts">
+      {posts.map(({
+        cover, title, id, body,
+      }) => (
+        <PostCard
+          key={id}
+          title={title}
+          cover={cover}
+          id={id}
+          body={body}
+        />
+      ))}
+    </div>
+  );
 }
+
+Posts.propTypes = {
+  posts: PropTypes.string.isRequired,
+};
 
 export default Posts;
